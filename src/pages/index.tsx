e0fc045d2c1d5b217cb0bf7222bf1b452d7c8b21
Kmiokande <brunnodesouzabezerra@gmail.com>
2021-04-20 15:19:34 -0300

    Using SSG on the page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,21 @@
-export default function Home() {
+export default function Home(props) {
   return (
-    <h1>Index</h1>
+    <div>
+      <h1>Index</h1>
+      <p>{JSON.stringify(props.episodes)}</p>
+    </div>
   )
+}
+
+// Estamos usando o método SSG (Static Site Generator)
+export async function getStaticProps() {
+  const response = await fetch('http://localhost:3333/episodes')
+  const data = await response.json()
+
+  return {
+    props: {
+      episodes: data,
+    },
+    revalidate: 60 * 60 * 8, // A chamada na API só será feita três vezes ao dia.
+  }
 }
